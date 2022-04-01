28367f5c10ff1a4d2865dd9bf9f8e7005125fb3e
JosVelazquez <ve415477@uaeh.edu.mx>
2022-03-31 19:36:59 -0600

    inicio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function suma (n1,n2){
+  var numero 1; = parseFloat(documnet.getElementsidByTid('numero1').value);
+  var numero 2; = parseFloat(documnet.getElementsidByTid('numero2').value);
+  var res; = n1+n2;
+
+  documnet.getElementsidByTid('resultao').value = res;
+
+}
